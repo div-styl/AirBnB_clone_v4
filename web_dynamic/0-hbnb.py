85e7244647a3aefc7c8319186115c8abd29ee3d7
adil mae <itsadilhadid@gmail.com>
2023-11-03 15:04:14 +0100

    update

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,38 +7,35 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
+import uuid
 
+app = Flask(__name__)
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def close_db_connection(error):
+    """ Close the current SQLAlchemy Session """
     storage.close()
 
-
 @app.route('/0-hbnb/', strict_slashes=False)
-def hbnb():
-    """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
+def display_hbnb():
+    """ Display the HBNB web page """
+    all_states = storage.all(State).values()
+    sorted_states = sorted(all_states, key=lambda k: k.name)
+    state_cities = []
 
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+    for state in sorted_states:
+        state_cities.append([state, sorted(state.cities, key=lambda k: k.name)])
 
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
+    all_amenities = storage.all(Amenity).values()
+    sorted_amenities = sorted(all_amenities, key=lambda k: k.name)
 
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
+    all_places = storage.all(Place).values()
+    sorted_places = sorted(all_places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
-                           places=places)
-
+    return render_template('100-hbnb.html',
+                           states=state_cities,
+                           amenities=sorted_amenities,
+                           places=sorted_places, cache_id=uuid.uuid4())
 
 if __name__ == "__main__":
     """ Main Function """
